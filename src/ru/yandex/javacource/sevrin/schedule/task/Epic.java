0899b77a2c9ad1da5c6369c8c895d54a391ba416
Ilya <i.sevrin@yandex.ru>
2025-04-01 09:58:43 +0300

    Внести стилистические изменения

--- src/ru/yandex/javacource/sevrin/schedule/task/Epic.java
+++ src/ru/yandex/javacource/sevrin/schedule/task/Epic.java
@@ -12,6 +12,7 @@ public class Epic extends Task {
     public Epic(int id, String title, String description, Status status) {
         super(id, title, description, status);
     }
+
     public ArrayList<Integer> getSubtaskIds() {
         return subtaskIds;
     }
